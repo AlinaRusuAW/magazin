62b078a0828026d7b23d328b5d36c4c7f76bc028
AlinaRusuAW <galaicuc213@gmail.com>
2020-11-17 00:24:56 +0200

    Cind utilizatorul isi ceeaza cont pe sit-ul nostru avem nevoie dea apune careva restrictii de lungimea a caracterelor, asta si o facem cu ajutorul instructiuni if

--- check.php
+++ check.php
@@ -2,4 +2,17 @@
 	$login = filter_var(trim($_POST['login']), FILTER_SANITIZE_STRING);
 	$name = filter_var(trim($_POST['name']), FILTER_SANITIZE_STRING);
 	$pass = filter_var(trim($_POST['pass']), FILTER_SANITIZE_STRING);
+
+		if(mb_strlen($login) < 4 || mb_strlen($login) > 90) {
+		echo "LUNGIME NEDISPONIBILĂ";
+		exit();
+	} elseif (mb_strlen($name) < 3 || mb_strlen($name) > 50) {
+		echo "LUNGIME NEDISPONIBILĂ";
+		exit(); 
+	} elseif (mb_strlen($pass) < 2 || mb_strlen($pass) > 6) {
+		echo "LUNGIME NEDISPONIBILĂ (de la 2 la 6 caractere)";
+		exit();
+	}
+
+
 ?>
