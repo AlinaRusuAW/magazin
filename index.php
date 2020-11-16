2bee02a8c614b6e867a6c15482dacc46b46d4dfa
AlinaRusuAW <galaicuc213@gmail.com>
2020-11-16 23:25:53 +0200

    Am creat primul fisier cu numele index.php care va fi pagina principala a magazinului nostru online de compozitii de flori

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="widht=divice-width, initial-scale=1.0">
+	<meta http-eqiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<title>Magazin online</title>
+</head>
+<body>
+	<h1>Situl meu</h1>
+</body>
+</html>
