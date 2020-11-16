1a5ccf534708b7863863628c4e13148e160946d8
AlinaRusuAW <galaicuc213@gmail.com>
2020-11-17 00:00:23 +0200

    In fisierul footer.php am adaugar un buton care ne intoarce Inapoi sus

--- blok/footer.php
+++ blok/footer.php
@@ -1,4 +1,5 @@
 <hr class="featurette-divider">
 <footer class="container">
+	<p class="float-right"><a href="#">Înapoi sus</a></p>
     <p>© 2019-2020 Magazin online "Kalisa" · Fondatorul companiei Rusu Alina ·  <a href="contacte.php">Contacte</a></p>
  </footer>
