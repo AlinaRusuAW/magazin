e7a353debb7d22068542551ec2a8761f98bd767a
AlinaRusuAW <galaicuc213@gmail.com>
2020-11-16 23:58:17 +0200

    Am adaugar in dosarul blok un fisier footer.php care reprezinta partea de jos a sitului (defaapt footer-ul)

---
+++ blok/footer.php
@@ -0,0 +1,4 @@
+<hr class="featurette-divider">
+<footer class="container">
+    <p>© 2019-2020 Magazin online "Kalisa" · Fondatorul companiei Rusu Alina ·  <a href="contacte.php">Contacte</a></p>
+ </footer>
