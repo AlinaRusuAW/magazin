87ac2f60283279e28ec6855ace187d613fa61823
AlinaRusuAW <galaicuc213@gmail.com>
2020-11-16 23:32:10 +0200

    Tot pe pagina principala am mai adaugat un bloc care este alcatuit din citeva blocuri mai mici, in care este descris si aratat produsul magazinului

--- index.php
+++ index.php
@@ -14,5 +14,84 @@
   			<p class="lead"> La noi poți găsi o selecție largă de produse - buchete dulci, compoziții florale şi buchețele mici, deasemenea suntem gata să realizăm la comandă orice creație doriți dvs. Produsele noastre vor fi cadoul perfect ce va readuce lumină și culori vii în zilele de rutină și vor crea o dispoziție de sărbătoare. </p>
 		</div>
 	</div>
+	<div class="container mt-5">
+<h3 class="mb-5"> Produsele noastre</h3>
+<div class="d-flex flex-wrap">
+	<div class="card mb-4 shadow-sm">
+     	 <div class="card-header">
+		        <h4 class="my-0 font-weight-normal">Buchet dulce în cutie "Imperial"</h4>
+		 </div>
+		      <div class="card-body">
+		        <img src="img/1.jpg" class="img-thumbnail">
+		        <ul class="list-unstyled mt-3 mb-4">
+		          <li>Flori din hîrtie creponată</li>
+		          <li>Culoarea: roșu și alb</li>
+		          <li>Categoria: Pentru femei</li>
+		          <li>Preț: 470 lei</li>
+		        </ul>
+		      	<button type="button" class="btn btn-lg btn-block btn-outline-success"><a href="comanda1.php">Detalii</a></button>
+      		  </div>
+    </div>
+<div class="card mb-4 shadow-sm">
+     	 <div class="card-header">
+		        <h4 class="my-0 font-weight-normal">Buchet Raffaello "Din Suflet"</h4>
+		 </div>
+		      <div class="card-body">
+		        <img src="img/2.jpg" class="img-thumbnail">
+		        <ul class="list-unstyled mt-3 mb-4">
+		          <li>Raffaello</li>
+		          <li>Culoarea: roșu și alb</li>
+		          <li>Categoria: Pentru femei</li>
+		          <li>Preț: 350 lei</li>
+		        </ul>
+		      	<button type="button" class="btn btn-lg btn-block btn-outline-success">Detalii</button>
+      		  </div>
+    </div>
+<div class="card mb-4 shadow-sm">
+     	 <div class="card-header">
+		        <h4 class="my-0 font-weight-normal">Buchet din fructe</h4>
+		 </div>
+		      <div class="card-body">
+		        <img src="img/3.jpg" class="img-thumbnail">
+		        <ul class="list-unstyled mt-3 mb-4">
+		          <li>Fructe</li>
+		          <li>Categoria: Pentru femei</li>
+		          <li>Preț: 250 lei</li>
+		        </ul>
+		      	<button type="button" class="btn btn-lg btn-block btn-outline-success">Detalii</button>
+      		  </div>
+    </div>
+<div class="card mb-4 shadow-sm">
+     	 <div class="card-header">
+		        <h4 class="my-0 font-weight-normal">Volan dulce "Audi"</h4>
+		 </div>
+		      <div class="card-body">
+		        <img src="img/4.jpg" class="img-thumbnail">
+		        <ul class="list-unstyled mt-3 mb-4">
+		          <li>Ferrero rocher</li>
+		          <li>Categoria: Pentru bărbați</li>
+		          <li>Preț: 380 lei</li>
+		        </ul>
+		      	<button type="button" class="btn btn-lg btn-block btn-outline-success">Detalii</button>
+      		  </div>
+    </div>
+<div class="card mb-4 shadow-sm">
+     	 <div class="card-header">
+		        <h4 class="my-0 font-weight-normal">Buchet dulce de lalele </h4>
+		 </div>
+		      <div class="card-body">
+		        <img src="img/5.jpg" class="img-thumbnail">
+		        <ul class="list-unstyled mt-3 mb-4">
+		          <li>Flori din hîrtie creponată</li>
+		          <li>Culoarea: roșu </li>
+		          <li>Categoria: Pentru femei</li>
+		          <li>Numărul de flori: 55 buc.</li>
+		          <li>Preț: 490 lei</li>
+		        </ul>
+		      	<button type="button" class="btn btn-lg btn-block btn-outline-success">Detalii</button>
+      		  </div>
+    </div>
+</div>
+</div>
 </body>
 </html>
