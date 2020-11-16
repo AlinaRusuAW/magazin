10ca0ec0a1b18fdc32c63aade814eb0e6656019f
AlinaRusuAW <galaicuc213@gmail.com>
2020-11-17 00:46:25 +0200

    Am scris codul cu denumirea antetului si cu butonul de inregistrare.

---
+++ forma.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="widht=divice-width, initial-scale=1.0">
+  <meta http-eqiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <title>Magazin online</title>
+</head>
+<body> 
+<?php require "blok/header.php" ?>
+<div class="container mt-5">  
+<form action="check.php" method="post">
+  <h1 class="h3 mb-3 font-weight-normal">Formă de înregistrare </h1>
+  
+  <button class="btn btn-lg btn-success btn-block" type="submit">Înregistrare</button> 
+</form>
+</div>
+<?php require "blok/footer.php" ?>
+</body>
+</html>
