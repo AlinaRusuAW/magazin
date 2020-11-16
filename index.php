2ab3e766fafa41452bb715f7c708b18e6cf84003
AlinaRusuAW <galaicuc213@gmail.com>
2020-11-16 23:28:58 +0200

    Am adaugat un bloc pe aceasta pagina, unde este descris succint despre produsele companii

--- index.php
+++ index.php
@@ -9,6 +9,10 @@
 	<title>Magazin online</title>
 </head>
 <body>
-	<h1>Situl meu</h1>
+	<div class="container mt-5">
+		<div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+  			<p class="lead"> La noi poți găsi o selecție largă de produse - buchete dulci, compoziții florale şi buchețele mici, deasemenea suntem gata să realizăm la comandă orice creație doriți dvs. Produsele noastre vor fi cadoul perfect ce va readuce lumină și culori vii în zilele de rutină și vor crea o dispoziție de sărbătoare. </p>
+		</div>
+	</div>
 </body>
 </html>
