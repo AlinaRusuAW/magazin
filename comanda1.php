a57adcc1b0d206c4495080f93d7d8758ba3fa4b0
AlinaRusuAW <galaicuc213@gmail.com>
2020-11-17 15:00:56 +0200

    Pe pagina detalii comanda am adaugat un bloc in care este scris codul programului unde se adauga comentarii. functionalul caruia urmeaza sa-l realizam.

--- comanda1.php
+++ comanda1.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<link rel="stylesheet" href="style.css">
-	 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="style.css">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-	<title>Magazin online</title>
+  <title>Magazin online</title>
 </head>
 <body>
 <?php require "blok/header.php"?>
@@ -16,21 +16,37 @@
         <h2 class="featurette-heading">Buchet dulce în cutie "Imperial"</span></h2>
         <p class="lead">
          <ul class="list-unstyled mt-3 mb-4">
-		          <li>-Flori din hîrtie creponată</li>
-		          <li>-Culoarea: roșu și alb (sau la alegerea dvs.)</li>
-		          <li>-Bomboane: Raffaello (sau la alegerea dvs.)</li>
-		          <li>-Categoria: Pentru femei</li>
-		          <li>-Preț: 470 lei</li>
-		        </ul>
-            <button type="button" class="btn btn-lg btn-block btn-outline-success"><a href="comanda1.php">Adaugă în coș</a></button><br>
-		    </p>
+              <li>-Flori din hîrtie creponată</li>
+              <li>-Culoarea: roșu și alb (sau la alegerea dvs.)</li>
+              <li>-Bomboane: Raffaello (sau la alegerea dvs.)</li>
+              <li>-Categoria: Pentru femei</li>
+              <li>-Preț: 470 lei</li>
+            </ul>
+            <button type="button" class="btn btn-lg btn-block btn-outline-success"><a href="comanda1.php">Adaugă în coș</a></button><br>     
+        </p>
       </div>
        <div class="col-md-5">
        <img src="img/1.jpg" class="img-thumbnail">
       </div>
     </div>
 </div>
-</div>
+<div class="container mt-3">
+    <h2 class="text-center">Comentarii:</h2>
+    <from action="" met>
+        <div class="from-grup">
+              <label for="comment-name">Nume:</label>
+              <input type="name" class="from-control" id="comment-name" placeholder="Nume">
+            </div>
+            <div class="from-grup">
+              <label for="comment-body">Comentariu dvs:</label>
+              <input type="name" class="from-control" id="comment-body" placeholder="Comentariu">
+            </div>
+            <div class="from-grup">
+              <button type="submit" id="comment-add" class="btn btn-success">Adauga com</button>
+            </div>
+          </from>
+
+    </div>
 </div>
 <?php require "blok/footer.php"?>
 </body>
