c3be2e36e982e13e1348b95c0e25f2de031aa2e9
AlinaRusuAW <galaicuc213@gmail.com>
2020-11-17 14:19:15 +0200

    Am adaugat pagina de contacte a magazinului

---
+++ contacte.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="widht=divice-width, initial-scale=1.0">
+  <meta http-eqiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <title>Magazin online</title>
+</head>
+<body> 
+<?php require "blok/header.php" ?>
+<div class="container">
+<div class="inform-box"> 
+	 <img src="img/contacte.jpg">
+		 <div class="basic">
+		 <div class="title">Vă așteptăm la noi.
+		 </div> 
+		  <div class="item"> <div class="level">Adresa noastră:</div>
+		   <div class="value"> <div class="i i-2">Bălți, str. I. Vatamanu 3A</div>
+		  </div> </div> 
+		  <div class="item"> <div class="level"> Contacte:  </div> 
+		   <div class="inl">  <img src="img/v.png" width="3.5%" height="3.5%">
+		   <a href="viber://add?number=37368558762" 
+		   class="tx"> +37368558762</a></div>
+		   <img src="img/f.png" width="4%" height="4%">
+		   <a href="www.facebook.com/alinarusu111" class="tx"> Facebook</a> </div> </div> </div>
+</div>
+</div>
+<?php require "blok/footer.php" ?>
+</body>
+</html>
