23cad5ce726b6c7afd1c13263a1f98430407d581
AlinaRusuAW <galaicuc213@gmail.com>
2020-11-17 14:36:04 +0200

    Am adaugat o pagina cu detalii pentru comanda primului produs, in care avem informatii despre produs si imaginea lui.

---
+++ comanda1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="style.css">
+	 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+	<title>Magazin online</title>
+</head>
+<body>
+<?php require "blok/header.php"?>
+<div class="container mt-5">
+    <hr class="featurette-divider">
+    <div class="row featurette">
+      <div class="col-md-7 order-md-2">
+        <h2 class="featurette-heading">Buchet dulce în cutie "Imperial"</span></h2>
+        <p class="lead">
+         <ul class="list-unstyled mt-3 mb-4">
+		          <li>-Flori din hîrtie creponată</li>
+		          <li>-Culoarea: roșu și alb (sau la alegerea dvs.)</li>
+		          <li>-Bomboane: Raffaello (sau la alegerea dvs.)</li>
+		          <li>-Categoria: Pentru femei</li>
+		          <li>-Preț: 470 lei</li>
+		        </ul>
+		    </p>
+      </div>
+       <div class="col-md-5">
+       <img src="img/1.jpg" class="img-thumbnail">
+      </div>
+    </div>
+</div>
+</div>
+</div>
+<?php require "blok/footer.php"?>
+</body>
+</html>
