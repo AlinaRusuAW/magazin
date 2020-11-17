f08e52ec2d37933e4676e9b03ad57a4fc0f280bf
AlinaRusuAW <galaicuc213@gmail.com>
2020-11-17 14:13:25 +0200

    Pe pagina Despre companie am adaugat foto la fiecare bolc existent, pentru a fi mai frumos si mai pe inteles despre ce se arata.

--- despre.php
+++ despre.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="widht=divice-width, initial-scale=1.0">
-	<meta http-eqiv="X-UA-Compatible" content="ie=edge">
-	<link rel="stylesheet" href="style.css">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-	<title>Magazin online</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="widht=divice-width, initial-scale=1.0">
+  <meta http-eqiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <title>Magazin online</title>
 </head>
 <body>
 <?php require "blok/header.php" ?>
@@ -17,20 +17,31 @@
         <h2 class="featurette-heading">Magazin Online "Kalisa". <span class="text-muted">Buchetele și compozițiile de dulciuri</span></h2>
         <p class="lead">Buchetele și compozițiile de dulciuri sunt o capodoperă realizată manual. Buchetul poartă dragoste, tandrețe și grijă. Prin achiziționarea buchetelor noastre, puteți fi siguri de designul excelent, prospețimea bomboanelor de ciocolată, calitatea materialelor utilizate în compoziții și respectarea termenelor limită. Oferim o abordare individuală fiecărui client! Valoarea noastră principală este încrederea dvs. </p>
       </div>
-	    </div>
+      <div class="col-md-5">
+         <img src="img/magazin.jpg" class="img-thumbnail">
+        </div>
+      </div>
     <hr class="featurette-divider">
     <div class="row featurette">
       <div class="col-md-7 order-md-2">
         <h2 class="featurette-heading">Calitate și siguranță la prețuri accesibile. <span class="text-muted">Gama noastră de produse</span></h2>
         <p class="lead">Buchetele și compozițiile magazinului nostru sunt realizate cu suflet și calitate superioară. Experții noștri transformă prin magie dulciurile și bomboanele în cadouri rafinate. Aici veți găsi o mare varietate de buchete de dulciuri, precum și cadouri pentru familia, prietenii și cei dragi pentru sărbători precum Anul Nou, 8 martie, Ziua Îndrăgostiților, 23 februarie, ziua de naștere, 1 septembrie, nunta și orice altă sărbătoare.</p>
       </div>
+       <div class="col-md-5">
+       <img src="img/flori.jpg" class="img-thumbnail">
+      </div>
     </div>
+
     <hr class="featurette-divider">
+
     <div class="row featurette">
       <div class="col-md-7">
         <h2 class="featurette-heading">Alina Rusu. <span class="text-muted">Fondatorul companiei magazin online "Kalisa"</span></h2>
         <p class="lead">Fondatorul și șeful magazinului online "Kalisa" este Alina Rusu. Magazinul nostru și-a început activitățile pe 07 martie 2019 și într-un an de muncă a devenit recunoscut în mun. Bălți și regiunile înconjurătoare. Producția de buchete și compoziții din dulciuri se desfășoară în mun. Bălți. <br> Buchetele de bomboane sunt un cadou la modă și delicios! Oferiți cadouri dulci cu Magazin Online "Kalisa" </p>
-      </div> 
+      </div>
+      <div class="col-md-5">
+       <img src="img/alina.jpg" class="img-thumbnail">
+      </div>
     </div>
   </div>
 <?php require "blok/footer.php" ?>
